e32703c36269a886170478cfa04a1cf2b209f06d
AndresC1 <andrescornejolira@gmail.com>
2023-06-10 13:17:09 -0600

    price plan list

---
+++ src/components/PlanComparation/planList.js
@@ -0,0 +1,22 @@
+const plansList = [
+    [
+        "Compare plans", "Encrypts included", 
+        "Decrypts included", "File Decrypts included", 
+        "Environments included", "Multiple Users per Team", 
+        "Multiple Teams", "Compliance Onboarding", 
+        "Solutions Engineer Support", "Dedicated Account Manager", 
+        "SLA for 99.99% Uptime", "Email Support", 
+        "Advanced Support Scope"
+    ],
+    [
+        "Free", "Unlimited", "5,000 p/m", "500 p/m", "1", "", "", "", "", "", "", "check", ""
+    ],
+    [
+        "Pro", "Unlimited", "15,000 p/m", "1,500 p/m", "Unlimited", "check", "", "", "", "", "", "check", ""
+    ],
+    [
+        "Enterprise", "Unlimited", "Custom", "Custom", "Unlimited", "check", "check", "check", "check", "check", "check", "check", "check"
+    ]
+]
+
+export default plansList;
