b9489800321fb1c90c252143c6a6c548e6d6d7e8
AndresC1 <andrescornejolira@gmail.com>
2023-06-10 16:13:45 -0600

    Contact list

---
+++ src/components/landing/contact/ContactData.js
@@ -0,0 +1,18 @@
+const dataButton = [
+    {
+        id: 1,
+        title: "PCI DSS",
+        subTitle: "Included with Enterprise plans",
+        description: "Any organisation that accepts or processes payment cards must validate compliance with PCI DSS. Evervault does the heavy lifting for PCI compliance allowing you to stay focused on building your core product.",
+        titleButton: "Contact Sales"
+    },
+    {
+        id: 2,
+        title: "PLL & HIPPA",
+        subTitle: "Included with All plans",
+        description: "Use Evervault to secure, process, and transmit Personally Identifiable Information (GDPR) and ePHI (HIPAA).",
+        titleButton: "Contact Sales"
+    }
+]
+
+export default dataButton
